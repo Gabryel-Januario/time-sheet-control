9be1c2c2855dccc7d77dfc0c16f8ce05526465cb
Gabryel Januario <gabryeelcomy@gmail.com>
2025-02-18 12:20:18 -0300

    create register service

--- src/main/java/com/time_sheet_control/time/sheet/control/service/AuthenticationService.java
+++ src/main/java/com/time_sheet_control/time/sheet/control/service/AuthenticationService.java
@@ -4,15 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.time_sheet_control.time.sheet.control.exceptions.UserAlreadyExistsException;
+import com.time_sheet_control.time.sheet.control.models.dto.authDTO.RegisterDTO;
+import com.time_sheet_control.time.sheet.control.models.users.User;
 import com.time_sheet_control.time.sheet.control.repositories.UserRepository;
 
 @Service
 public class AuthenticationService implements UserDetailsService {
     
     @Autowired
-    UserRepository repository;
+    private UserRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -20,6 +24,18 @@ public class AuthenticationService implements UserDetailsService {
         return this.repository.findByLogin(username);
     }
 
+    public User register(RegisterDTO data) {
+        if(this.repository.findByLogin(data.login()) != null) {
+            throw new UserAlreadyExistsException("User Already Exists with this email!");
+        }
 
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+
+        User newUser = new User(data.name(), data.login(), encryptedPassword, data.position(), data.role());
+
+        this.repository.save(newUser);
+
+        return newUser;
+    }
     
 }
