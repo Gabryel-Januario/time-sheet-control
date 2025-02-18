7c12ffc85a7a450fc316ea5fc73c99773d543f2a
Gabryel Januario <gabryeelcomy@gmail.com>
2025-02-18 12:18:16 -0300

    create registerUser

---
+++ src/main/java/com/time_sheet_control/time/sheet/control/controllers/AuthController.java
@@ -0,0 +1,30 @@
+package com.time_sheet_control.time.sheet.control.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.time_sheet_control.time.sheet.control.models.dto.authDTO.RegisterDTO;
+
+import com.time_sheet_control.time.sheet.control.service.AuthenticationService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    
+    @Autowired
+    private AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterDTO data) {
+
+        this.service.register(data);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+
+    }
+}
