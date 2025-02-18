2da81a0e3563ba9c26e93c206cf0a4da42defca5
Gabryel Januario <gabryeelcomy@gmail.com>
2025-02-18 09:34:50 -0300

    Frist commit

---
+++ src/main/java/com/time_sheet_control/time/sheet/control/TimeSheetControlApplication.java
@@ -0,0 +1,13 @@
+package com.time_sheet_control.time.sheet.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSheetControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSheetControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/time_sheet_control/time/sheet/control/TimeSheetControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time_sheet_control.time.sheet.control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSheetControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
