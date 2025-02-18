355e0bc4e0dd262042b7b73bdbaf3895a9ce2f58
Gabryel Januario <gabryeelcomy@gmail.com>
2025-02-18 10:07:08 -0300

    created authenticationService

---
+++ src/main/java/com/time_sheet_control/time/sheet/control/service/AuthenticationService.java
@@ -0,0 +1,25 @@
+package com.time_sheet_control.time.sheet.control.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.time_sheet_control.time.sheet.control.repositories.UserRepository;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+    
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+        return this.repository.findByLogin(username);
+    }
+
+
+    
+}
