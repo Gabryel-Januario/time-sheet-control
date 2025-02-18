79f586c595e63e1a5d7ff392700747f26559c478
Gabryel Januario <gabryeelcomy@gmail.com>
2025-02-18 10:01:27 -0300

    set anotations

--- src/main/java/com/time_sheet_control/time/sheet/control/models/users/User.java
+++ src/main/java/com/time_sheet_control/time/sheet/control/models/users/User.java
@@ -7,7 +7,11 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -16,14 +20,20 @@ import lombok.Data;
 @Table(name = "users")
 public class User implements UserDetails{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-
+    
+    @Column(nullable = false, unique = true)
     private String login;
-
+    
+    @Column(nullable = false)
     private String password;
-
+    
+    @Column(nullable = false)
     private String position;
-
+    
+    @Column(nullable = false)
     private UserRole role;
 
     @Override
