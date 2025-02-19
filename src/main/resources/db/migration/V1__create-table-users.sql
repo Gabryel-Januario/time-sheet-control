3033872a358e595698d7072257c9c889d19ba347
Gabryel Januario <gabryeelcomy@gmail.com>
2025-02-19 12:40:14 -0300

    change email for login

--- src/main/resources/db/migration/V1__create-table-users.sql
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
+    login TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     position TEXT NOT NULL,
     role TEXT NOT NULL
