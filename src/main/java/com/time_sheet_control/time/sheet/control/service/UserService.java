969b0a33c1c8d4a1c7dc54ec0f87389989879356
Gabryel Januario <gabryeelcomy@gmail.com>
2025-02-25 10:43:33 -0300

    create user getById service

--- src/main/java/com/time_sheet_control/time/sheet/control/service/UserService.java
+++ src/main/java/com/time_sheet_control/time/sheet/control/service/UserService.java
@@ -1,11 +1,14 @@
 package com.time_sheet_control.time.sheet.control.service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.time_sheet_control.time.sheet.control.exceptions.ElementNotFound;
+import com.time_sheet_control.time.sheet.control.models.dto.users.UserResponseDTO;
 import com.time_sheet_control.time.sheet.control.models.dto.users.UsersDTO;
 import com.time_sheet_control.time.sheet.control.models.users.User;
 import com.time_sheet_control.time.sheet.control.repositories.UserRepository;
@@ -22,6 +25,18 @@ public class UserService {
         if(users.isEmpty()) throw new ElementNotFound("Users not found");
 
         return users.stream().map(user -> new UsersDTO(user.getName(), user.getLogin(), user.getPosition(), user.getRole())).toList();
+    }
+
+    public UserResponseDTO getById(String id) {
+        Optional<User> user = this.repository.findById(id);
+        
+        if(!user.isPresent()) throw new ElementNotFound("User not found");
+
+        UserResponseDTO response = new UserResponseDTO(user.get().getName(), user.get().getLogin(), user.get().getPosition(), user.get().getRole());
+
+
+        return response;
+
     }
 
 }
