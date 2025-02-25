22ef36938ed02c3cbc4872f25ea746abee1290e1
Gabryel Januario <gabryeelcomy@gmail.com>
2025-02-25 10:44:37 -0300

    create response for user data

---
+++ src/main/java/com/time_sheet_control/time/sheet/control/models/dto/users/UserResponseDTO.java
@@ -0,0 +1,23 @@
+package com.time_sheet_control.time.sheet.control.models.dto.users;
+
+import com.time_sheet_control.time.sheet.control.models.users.UserRole;
+
+import lombok.Data;
+
+@Data
+public class UserResponseDTO {
+    
+    private String name;
+    private String login;
+    private String position;
+    private UserRole role;
+
+    public UserResponseDTO(String name, String login, String position, UserRole role) {
+        this.name = name;
+        this.login = login;
+        this.position = position;
+        this.role = role;
+    }
+
+
+}
