10f38f85aebfde0e95baea91e77bbd2c05d4f778
Gabryel Januario <gabryeelcomy@gmail.com>
2025-02-25 10:23:01 -0300

    create elementNotFound exception

---
+++ src/main/java/com/time_sheet_control/time/sheet/control/exceptions/ElementNotFound.java
@@ -0,0 +1,8 @@
+package com.time_sheet_control.time.sheet.control.exceptions;
+
+public class ElementNotFound extends RuntimeException{
+    
+    public ElementNotFound(String message) {
+        super(message);
+    }
+}
