7ff7a41f24892bd55879827d6018477f60b1007e
Gabryel Januario <gabryeelcomy@gmail.com>
2025-02-25 10:23:35 -0300

    set elementnotfound in global

--- src/main/java/com/time_sheet_control/time/sheet/control/infra/GlobalExceptionHandler.java
+++ src/main/java/com/time_sheet_control/time/sheet/control/infra/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.time_sheet_control.time.sheet.control.exceptions.ElementNotFound;
 import com.time_sheet_control.time.sheet.control.exceptions.UserAlreadyExistsException;
 
 @RestControllerAdvice
@@ -14,5 +15,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<String> HandleUserAlreadyExistsException(UserAlreadyExistsException exception) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(ElementNotFound.class)
+    public ResponseEntity<String> HandlerElementNotFound(ElementNotFound exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 }
