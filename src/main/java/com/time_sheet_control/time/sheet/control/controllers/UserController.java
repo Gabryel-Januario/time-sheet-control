5767445630bb3985cc2e4376c3c4724b72e862c1
Gabryel Januario <gabryeelcomy@gmail.com>
2025-02-25 10:46:09 -0300

    set getAll/getById service in endpoints

--- src/main/java/com/time_sheet_control/time/sheet/control/controllers/UserController.java
+++ src/main/java/com/time_sheet_control/time/sheet/control/controllers/UserController.java
@@ -1,30 +1,41 @@
 package com.time_sheet_control.time.sheet.control.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.time_sheet_control.time.sheet.control.models.dto.users.UserResponseDTO;
+import com.time_sheet_control.time.sheet.control.models.dto.users.UsersDTO;
+import com.time_sheet_control.time.sheet.control.service.UserService;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 
-    @PostMapping("/create")
-    public ResponseEntity<String> createUser() {
-        return ResponseEntity.ok("User created successfully");
-    }
-
+    @Autowired
+    private UserService service;
+     
     @GetMapping("/all")
-    public ResponseEntity<String> getAllUsers() {
-        return ResponseEntity.ok("All users here");
+    public ResponseEntity<List<UsersDTO>> getAllUsers() {
+
+        List<UsersDTO> users = this.service.getAll();
+
+        return ResponseEntity.status(HttpStatus.FOUND).body(users);
     }
 
     @GetMapping("/user/{id}")
-    public ResponseEntity<String> userById() {
-        return ResponseEntity.ok("User by id here");
+    public ResponseEntity<UserResponseDTO> userById(@PathVariable String id ) {
+
+        UserResponseDTO user = this.service.getById(id);
+
+        return ResponseEntity.status(HttpStatus.FOUND).body(user);
     }
 
     @PutMapping("/user/{id}")
