fc89665c7cb6d108df955a3fd6054916ce3b3e4d
Gabryel Januario <gabryeelcomy@gmail.com>
2025-02-25 15:24:52 -0300

    set onetomany with timerecords

--- src/main/java/com/time_sheet_control/time/sheet/control/models/users/User.java
+++ src/main/java/com/time_sheet_control/time/sheet/control/models/users/User.java
@@ -7,6 +7,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.time_sheet_control.time.sheet.control.models.timerecords.TimeRecords;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -14,6 +16,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -41,6 +44,9 @@ public class User implements UserDetails{
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private UserRole role;
+
+    @OneToMany(mappedBy = "user")
+    private List<TimeRecords> timeRecords;
 
     public User() {}
 
